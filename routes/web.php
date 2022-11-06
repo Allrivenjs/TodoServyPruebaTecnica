496ace37a179508f1fd00a4b1bbde017ec683ce8
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2022-11-05 19:46:25 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [Controller::class, 'getImages'])
     ->name('getAnyImage');
+
+Route::get('/', function () {
+    return view('welcome');
+});
